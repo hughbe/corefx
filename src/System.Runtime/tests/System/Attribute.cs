3b3d23b3e1d945e7c6d242977fc0d41418bfadba
Hugh Bellamy <hughbellars@gmail.com>
2016-01-17 12:51:51 +0000

    Cleanup Attribute Tests

--- src/System.Runtime/tests/System/Attribute.cs
+++ src/System.Runtime/tests/System/Attribute.cs
@@ -1,35 +1,36 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Reflection;
 using System.Linq;
 
 using Xunit;
 
-public static unsafe class AttributeTests
+namespace System.Runtime.Tests
 {
-    [ActiveIssue("https://github.com/dotnet/coreclr/issues/2037", PlatformID.AnyUnix)]
-    [Fact]
-    [StringValue("\uDFFF")]
-    public static void StringArgument_InvalidCodeUnits_FallbackUsed()
+    public static class AttributeTests
     {
-        MethodInfo thisMethod = typeof(AttributeTests).GetTypeInfo().GetDeclaredMethod("StringArgument_InvalidCodeUnits_FallbackUsed");
-        Assert.NotNull(thisMethod);
+        [ActiveIssue("https://github.com/dotnet/coreclr/issues/2037", PlatformID.AnyUnix)]
+        [Fact]
+        [StringValue("\uDFFF")]
+        public static void StringArgument_InvalidCodeUnits_FallbackUsed()
+        {
+            MethodInfo thisMethod = typeof(AttributeTests).GetTypeInfo().GetDeclaredMethod("StringArgument_InvalidCodeUnits_FallbackUsed");
+            Assert.NotNull(thisMethod);
 
-        CustomAttributeData cad = thisMethod.CustomAttributes.Where(ca => ca.AttributeType == typeof(StringValueAttribute)).FirstOrDefault();
-        Assert.NotNull(cad);
+            CustomAttributeData cad = thisMethod.CustomAttributes.Where(ca => ca.AttributeType == typeof(StringValueAttribute)).FirstOrDefault();
+            Assert.NotNull(cad);
 
-        string stringArg = cad.ConstructorArguments[0].Value as string;
-        Assert.NotNull(stringArg);
+            string stringArg = cad.ConstructorArguments[0].Value as string;
+            Assert.NotNull(stringArg);
+            Assert.Equal("\uFFFD\uFFFD", stringArg);
+        }
 
-        Assert.Equal("\uFFFD\uFFFD", stringArg);
-    }
-
-    [AttributeUsage(AttributeTargets.Method)]
-    private sealed class StringValueAttribute : Attribute
-    {
-        public string Text;
-        public StringValueAttribute(string text) { Text = text; }
+        [AttributeUsage(AttributeTargets.Method)]
+        private sealed class StringValueAttribute : Attribute
+        {
+            public string Text;
+            public StringValueAttribute(string text) { Text = text; }
+        }
     }
 }
